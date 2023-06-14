0823c62ae345e09763f8c6f3445101183b06d9b5
Sarraf Rahman <srrfrhmn@gmail.com>
2023-06-13 22:50:59 -0400

    shortener service complete

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -2,10 +2,38 @@ package shortener
 
 import (
 	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/itchyny/base58-go"
 )
 
 func sha256Of(input string) []byte {
 	algo := sha256.New()
 	algo.Write([]byte(input))
 	return algo.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+func GenerateShortURL(initialLink string, userId string) string {
+	// Hashing the initial link + the user id
+	urlHashBytes := sha256Of(initialLink + userId)
+
+	// Deriving a big integer number from the hash bytes
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+
+	// Converting the hash bytes to a base58 encoded string and returning the first 8 characters
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+
+	return finalString[:8]
 }
--- shortener/shorturl_generator_test.go
+++ shortener/shorturl_generator_test.go
@@ -1,1 +1,24 @@
 package shortener
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const UserId = "e0dba740-fc4b-4977-872c-d360239e6b1a"
+
+func TestShortLinkGenerator(t *testing.T) {
+	initalLink_1 := "https://www.apple.com/ca/business/enterprise/"
+	shortLink_1 := GenerateShortURL(initalLink_1, UserId)
+
+	initialLink_2 := "https://about.google/"
+	shortLink_2 := GenerateShortURL(initialLink_2, UserId)
+
+	initialLink_3 := "https://www.microsoft.com/en-ca/about/"
+	shortLink_3 := GenerateShortURL(initialLink_3, UserId)
+
+	assert.Equal(t, "J9sgTr2T", shortLink_1)
+	assert.Equal(t, "7Ki5z8PN", shortLink_2)
+	assert.Equal(t, "PyjxzyzS", shortLink_3)
+}
